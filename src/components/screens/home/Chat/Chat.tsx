7f0fcbfb7bebf3bd19b83029c3b705e1756cdab1
Andrew Hrechkan <andrewhrechkan@gmail.com>
2023-06-20 19:25:00 +0300

    fix/loading

--- src/components/screens/home/Chat/Chat.tsx
+++ src/components/screens/home/Chat/Chat.tsx
@@ -27,9 +27,9 @@ export const Chat: FC = () => {
 	};
 
 	useEffect(() => {
+		setLoading(true);
 		CommentService.getComments()
 			.then(({ comments }) => {
-				setLoading(true);
 				setComments(comments);
 			})
 			.catch(err => {
